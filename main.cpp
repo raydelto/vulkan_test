4560443623810537993ea4d77dcde59567f09b5f
Raydelto Hernandez <raydelto@gmail.com>
2024-12-05 06:13:36 -0400

    Polishing main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@
 #include <stdexcept>
 #include <vector>
 #include <iostream>
+#include <cstdlib>
 
 #include "VulkanRenderer.h"
 
-GLFWwindow * window;
+GLFWwindow *window;
 VulkanRenderer vulkanRenderer;
 
 void initWindow(std::string wName = "Test Window", const int width = 800, const int height = 600)
 {
-	// Initialise GLFW
+	// Initialize GLFW
 	glfwInit();
 
 	// Set GLFW to NOT work with OpenGL
@@ -52,13 +53,14 @@ int main()
 		lastTime = now;
 
 		angle += 10.0f * deltaTime;
-		if (angle > 360.0f) { angle -= 360.0f; }
-        glm::mat4  translation = glm::translate(glm::mat4(1.0f), glm::vec3(-30.0f, -20.0f, -75.0f));
-		glm::mat4 testMat = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
-		//testMat = glm::rotate(testMat, glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-        testMat = translation * testMat;
-		vulkanRenderer.updateModel(helicopter, testMat);
-
+		if (angle >= 360.0f)
+		{
+			angle = 0.0f;
+		}
+		glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(-30.0f, -20.0f, -75.0f));
+		glm::mat4 transformMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+		transformMatrix = translation * transformMatrix;
+		vulkanRenderer.updateModel(helicopter, transformMatrix);
 		vulkanRenderer.draw();
 	}
 
@@ -68,5 +70,5 @@ int main()
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
-	return 0;
+	return EXIT_SUCCESS;
 }
